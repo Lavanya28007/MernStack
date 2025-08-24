7acfa8addace1de6306f89ea9af222d737305ec3
lavanya28007 <lavanyasahni208@outlook.com>
2025-08-24 20:46:21 +0530

    model

---
+++ Section 3/Backend/connection.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const url = 'mongodb+srv://lavanya:abcd123@cluster0.4l6fts5.mongodb.net/mydb1100?retryWrites=true&w=majority&appName=Cluster0'
+
+//asynchronous function - promise object
+mongoose.connect(url)
+.then((result) => {
+    console.log('database connected');    
+    // console.log(result);
+    
+}).catch((err) => {
+    console.log(err);    
+});
+
+
+module.exports = mongoose;
